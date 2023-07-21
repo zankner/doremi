0e1211176508ccba658820248cbf35e6eaec6767
Zack Ankner <zackankner@gmail.com>
2023-07-21 13:45:48 -0700

    Add constants.sh file with sample environment variables

---
+++ constants.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This is a sample of the constants file. Please write any env variables here
+# and rename the file constants.sh
+
+CACHE=/tmp/doremi/cache
+DOREMI_DIR=/doremi
+MODEL_OUTPUT_DIR=/tmp/doremi/models
+mkdir -p ${CACHE}
+mkdir -p ${MODEL_OUTPUT_DIR}
