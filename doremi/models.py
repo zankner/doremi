1284a5601fd8ec1a2e3f10fd216903a3680b8675
Zack Ankner <zackankner@gmail.com>
2023-07-23 10:46:18 -0700

    final model training

--- doremi/models.py
+++ doremi/models.py
@@ -17,154 +17,155 @@ try:
 except Exception:
     from torch.nn import CrossEntropyLoss
 
-
 import logging
 
 logger = logging.getLogger(__name__)
 
-
 #@dataclass
 #class CausalLMOutputWithDomainIDs(CausalLMOutputWithCrossAttentions):
-    #domain_ids: Optional[torch.LongTensor] = None
-    #reference_pertoken_loss: Optional[torch.FloatTensor] = None  # corresponds to uniq_domain_ids
-    #pertoken_loss: Optional[torch.FloatTensor] = None  # corresponds to uniq_domain_ids
-    #token_mask: Optional[torch.BoolTensor] = None  # 1 for tokens that are not padding
-
+#domain_ids: Optional[torch.LongTensor] = None
+#reference_pertoken_loss: Optional[torch.FloatTensor] = None  # corresponds to uniq_domain_ids
+#pertoken_loss: Optional[torch.FloatTensor] = None  # corresponds to uniq_domain_ids
+#token_mask: Optional[torch.BoolTensor] = None  # 1 for tokens that are not padding
 
 #class GPTFlashAttnLMHeadModel(GPTLMHeadModelFlash):
 
-    #def __init__(self, config, process_group=None, device=None, dtype=None):
-        #super().__init__(config, process_group=process_group, device=device, dtype=dtype)
-        #self.ignore_index = -100
-        #self.loss_fct = CrossEntropyLoss(reduction='mean', ignore_index=self.ignore_index)
-        #self.pertoken_loss_fct = CrossEntropyLoss(reduction='none', ignore_index=self.ignore_index)
-        #self.reference_model = None
+#def __init__(self, config, process_group=None, device=None, dtype=None):
+#super().__init__(config, process_group=process_group, device=device, dtype=dtype)
+#self.ignore_index = -100
+#self.loss_fct = CrossEntropyLoss(reduction='mean', ignore_index=self.ignore_index)
+#self.pertoken_loss_fct = CrossEntropyLoss(reduction='none', ignore_index=self.ignore_index)
+#self.reference_model = None
 
-    #def forward(
-        #self,
-        #input_ids: Optional[torch.LongTensor] = None,
-        #attention_mask: Optional[torch.FloatTensor] = None,
-        #inputs_embeds: Optional[torch.FloatTensor] = None,
-        #head_mask: Optional[torch.FloatTensor] = None,
-        #past_key_values: Optional[Tuple[Tuple[torch.FloatTensor]]] = None,
-        #labels: Optional[torch.LongTensor] = None,
-        #use_cache: Optional[bool] = None,
-        #output_attentions: Optional[bool] = None,
-        #output_hidden_states: Optional[bool] = None,
-        #return_dict: Optional[bool] = None,
-        #domain_ids: Optional[torch.LongTensor] = None,
-        #return_pertoken_losses: Optional[bool] = False,
-    #) -> Union[Tuple, CausalLMOutputWithDomainIDs]:
-        #if not return_pertoken_losses:
-            #lm_logits = super().forward(input_ids=input_ids).logits
+#def forward(
+#self,
+#input_ids: Optional[torch.LongTensor] = None,
+#attention_mask: Optional[torch.FloatTensor] = None,
+#inputs_embeds: Optional[torch.FloatTensor] = None,
+#head_mask: Optional[torch.FloatTensor] = None,
+#past_key_values: Optional[Tuple[Tuple[torch.FloatTensor]]] = None,
+#labels: Optional[torch.LongTensor] = None,
+#use_cache: Optional[bool] = None,
+#output_attentions: Optional[bool] = None,
+#output_hidden_states: Optional[bool] = None,
+#return_dict: Optional[bool] = None,
+#domain_ids: Optional[torch.LongTensor] = None,
+#return_pertoken_losses: Optional[bool] = False,
+#) -> Union[Tuple, CausalLMOutputWithDomainIDs]:
+#if not return_pertoken_losses:
+#lm_logits = super().forward(input_ids=input_ids).logits
 
-            ## move labels to correct device to enable model parallelism
-            #labels = labels.to(lm_logits.device)
-            ## Shift so that tokens < n predict n
-            #shift_logits = lm_logits[:, :-1, :].contiguous()
-            #shift_labels = labels[:, 1:].contiguous()
-            ## Flatten the tokens
-            #loss = self.loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
+## move labels to correct device to enable model parallelism
+#labels = labels.to(lm_logits.device)
+## Shift so that tokens < n predict n
+#shift_logits = lm_logits[:, :-1, :].contiguous()
+#shift_labels = labels[:, 1:].contiguous()
+## Flatten the tokens
+#loss = self.loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
 
-            #return CausalLMOutputWithDomainIDs(
-                #loss=loss,
-                #logits=lm_logits,
-                #past_key_values=None,
-                #hidden_states=None,
-                #attentions=None,
-                #domain_ids=domain_ids)
-        #else:
-            #return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+#return CausalLMOutputWithDomainIDs(
+#loss=loss,
+#logits=lm_logits,
+#past_key_values=None,
+#hidden_states=None,
+#attentions=None,
+#domain_ids=domain_ids)
+#else:
+#return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
-            #lm_logits = super().forward(input_ids=input_ids).logits
+#lm_logits = super().forward(input_ids=input_ids).logits
 
-            #loss = None
-            #pertoken_loss = None
-            #reference_pertoken_loss = None
-            #if labels is not None:
-                ## move labels to correct device to enable model parallelism
-                #labels = labels.to(lm_logits.device)
-                ## Shift so that tokens < n predict n
-                #shift_logits = lm_logits[:, :-1, :].contiguous()
-                #shift_labels = labels[:, 1:].contiguous()
-                ## Flatten the tokens
-                #ignore_index = -100
-                #pertoken_loss = self.pertoken_loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
-                #pertoken_loss = pertoken_loss.view(shift_labels.size(0), shift_labels.size(1))
-                #token_mask = shift_labels.ne(ignore_index).float()
+#loss = None
+#pertoken_loss = None
+#reference_pertoken_loss = None
+#if labels is not None:
+## move labels to correct device to enable model parallelism
+#labels = labels.to(lm_logits.device)
+## Shift so that tokens < n predict n
+#shift_logits = lm_logits[:, :-1, :].contiguous()
+#shift_labels = labels[:, 1:].contiguous()
+## Flatten the tokens
+#ignore_index = -100
+#pertoken_loss = self.pertoken_loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
+#pertoken_loss = pertoken_loss.view(shift_labels.size(0), shift_labels.size(1))
+#token_mask = shift_labels.ne(ignore_index).float()
 
-                #loss = pertoken_loss.sum() / token_mask.sum()
+#loss = pertoken_loss.sum() / token_mask.sum()
 
-                ## run reference model forward to get pertoken_loss
-                #if self.reference_model is not None:
-                    #self.reference_model.eval()
-                    #reference_outputs = self.reference_model(
-                        #input_ids=input_ids,
-                        #attention_mask=attention_mask,
-                        #inputs_embeds=inputs_embeds,
-                        #head_mask=head_mask,
-                        #past_key_values=past_key_values,
-                        #labels=labels,
-                        #use_cache=use_cache,
-                        #output_attentions=output_attentions,
-                        #output_hidden_states=output_hidden_states,
-                        #return_dict=return_dict,
-                        #domain_ids=domain_ids,
-                        #return_pertoken_losses=True,
-                    #)
-                    #reference_pertoken_loss = reference_outputs['pertoken_loss']
+## run reference model forward to get pertoken_loss
+#if self.reference_model is not None:
+#self.reference_model.eval()
+#reference_outputs = self.reference_model(
+#input_ids=input_ids,
+#attention_mask=attention_mask,
+#inputs_embeds=inputs_embeds,
+#head_mask=head_mask,
+#past_key_values=past_key_values,
+#labels=labels,
+#use_cache=use_cache,
+#output_attentions=output_attentions,
+#output_hidden_states=output_hidden_states,
+#return_dict=return_dict,
+#domain_ids=domain_ids,
+#return_pertoken_losses=True,
+#)
+#reference_pertoken_loss = reference_outputs['pertoken_loss']
 
-            #if not return_dict:
-                #output = (lm_logits, None, None, None, domain_ids, pertoken_loss, reference_pertoken_loss, token_mask) 
-                #return ((loss,) + output) if loss is not None else output
+#if not return_dict:
+#output = (lm_logits, None, None, None, domain_ids, pertoken_loss, reference_pertoken_loss, token_mask)
+#return ((loss,) + output) if loss is not None else output
 
-            #return CausalLMOutputWithDomainIDs(
-                #loss=loss,
-                #logits=lm_logits,
-                #past_key_values=None,
-                #hidden_states=None,
-                #attentions=None,
-                #domain_ids=domain_ids,
-                #pertoken_loss=pertoken_loss,
-                #reference_pertoken_loss=reference_pertoken_loss,
-                #token_mask=token_mask)
+#return CausalLMOutputWithDomainIDs(
+#loss=loss,
+#logits=lm_logits,
+#past_key_values=None,
+#hidden_states=None,
+#attentions=None,
+#domain_ids=domain_ids,
+#pertoken_loss=pertoken_loss,
+#reference_pertoken_loss=reference_pertoken_loss,
+#token_mask=token_mask)
 
-    #@classmethod
-    #def from_pretrained(cls, model_name, config, *args, strict=True, device=None, dtype=None,
-                        #world_size=1, rank=0, **kwargs):
-        #"""
-        #Instantiate a GPTPreTrainedModel from a pre-trained model file or a pytorch state dict.
-        #Download and cache the pre-trained model file if needed.
-        #"""
-        ## Instantiate model.
-        #model = cls(config, *args, device=device, dtype=dtype, **kwargs)
-        ## Load state_dict in cpu because we already initialized the model in GPU, and we don't
-        ## want extra stuff taking up more GPU memory
-        #state_dict = state_dict_from_pretrained(model_name, device='cpu', dtype=dtype)
-        #if model_name.startswith('gpt2'):
-            #state_dict = remap_state_dict_hf_gpt2(state_dict, config)
-        #elif model_name.startswith('facebook/opt'):
-            #state_dict = remap_state_dict_hf_opt(state_dict, config)
-        #elif model_name.startswith('EleutherAI/gpt-j-'):
-            #state_dict = remap_state_dict_hf_gptj(state_dict, config)
-            #strict = False  # We have rotary_emb.inf_freq buffers not in the GPT-J checkpoint
-        #elif model_name.startswith('EleutherAI/gpt-neox-') or model_name.startswith('EleutherAI/pythia-'):
-            #state_dict = remap_state_dict_hf_gpt_neox(state_dict, config)
-        #else:
-            #pass
+#@classmethod
+#def from_pretrained(cls, model_name, config, *args, strict=True, device=None, dtype=None,
+#world_size=1, rank=0, **kwargs):
+#"""
+#Instantiate a GPTPreTrainedModel from a pre-trained model file or a pytorch state dict.
+#Download and cache the pre-trained model file if needed.
+#"""
+## Instantiate model.
+#model = cls(config, *args, device=device, dtype=dtype, **kwargs)
+## Load state_dict in cpu because we already initialized the model in GPU, and we don't
+## want extra stuff taking up more GPU memory
+#state_dict = state_dict_from_pretrained(model_name, device='cpu', dtype=dtype)
+#if model_name.startswith('gpt2'):
+#state_dict = remap_state_dict_hf_gpt2(state_dict, config)
+#elif model_name.startswith('facebook/opt'):
+#state_dict = remap_state_dict_hf_opt(state_dict, config)
+#elif model_name.startswith('EleutherAI/gpt-j-'):
+#state_dict = remap_state_dict_hf_gptj(state_dict, config)
+#strict = False  # We have rotary_emb.inf_freq buffers not in the GPT-J checkpoint
+#elif model_name.startswith('EleutherAI/gpt-neox-') or model_name.startswith('EleutherAI/pythia-'):
+#state_dict = remap_state_dict_hf_gpt_neox(state_dict, config)
+#else:
+#pass
 
-        #if world_size > 1:
-            #state_dict = shard_state_dict_tp(state_dict, config, world_size, rank)
-        #load_return = model.load_state_dict(state_dict, strict=strict)
-        #logger.info(load_return)
-        #return model
+#if world_size > 1:
+#state_dict = shard_state_dict_tp(state_dict, config, world_size, rank)
+#load_return = model.load_state_dict(state_dict, strict=strict)
+#logger.info(load_return)
+#return model
+
 
 @dataclass
 class CausalLMOutputWithDomainIDs(CausalLMOutputWithCrossAttentions):
     domain_ids: Optional[torch.LongTensor] = None
-    reference_pertoken_loss: Optional[torch.FloatTensor] = None  # corresponds to uniq_domain_ids
-    pertoken_loss: Optional[torch.FloatTensor] = None  # corresponds to uniq_domain_ids
-    token_mask: Optional[torch.BoolTensor] = None  # 1 for tokens that are not padding
+    reference_pertoken_loss: Optional[
+        torch.FloatTensor] = None  # corresponds to uniq_domain_ids
+    pertoken_loss: Optional[
+        torch.FloatTensor] = None  # corresponds to uniq_domain_ids
+    token_mask: Optional[
+        torch.BoolTensor] = None  # 1 for tokens that are not padding
 
 
 class MPTModel(ComposerMPTCausalLM):
@@ -172,8 +173,10 @@ class MPTModel(ComposerMPTCausalLM):
     def __init__(self, config, tokenizer):
         super().__init__(config, tokenizer)
         self.ignore_index = -100
-        self.loss_fct = CrossEntropyLoss(reduction='mean', ignore_index=self.ignore_index)
-        self.pertoken_loss_fct = CrossEntropyLoss(reduction='none', ignore_index=self.ignore_index)
+        self.loss_fct = CrossEntropyLoss(reduction='mean',
+                                         ignore_index=self.ignore_index)
+        self.pertoken_loss_fct = CrossEntropyLoss(
+            reduction='none', ignore_index=self.ignore_index)
         self.reference_model = None
 
     def forward(
@@ -190,11 +193,14 @@ class MPTModel(ComposerMPTCausalLM):
         return_dict: Optional[bool] = None,
         domain_ids: Optional[torch.LongTensor] = None,
         return_pertoken_losses: Optional[bool] = False,
-        tokens= None, # So that the trainer doesn't rm before collator,
-        sequence_id= None,
+        tokens=None,  # So that the trainer doesn't rm before collator,
+        sequence_id=None,
     ) -> Union[Tuple, CausalLMOutputWithDomainIDs]:
         if not return_pertoken_losses:
-            lm_logits = super().forward({"input_ids": input_ids, "sequence_id": sequence_id}).logits
+            lm_logits = super().forward({
+                "input_ids": input_ids,
+                "sequence_id": sequence_id
+            }).logits
 
             # move labels to correct device to enable model parallelism
             labels = labels.to(lm_logits.device)
@@ -202,19 +208,22 @@ class MPTModel(ComposerMPTCausalLM):
             shift_logits = lm_logits[:, :-1, :].contiguous()
             shift_labels = labels[:, 1:].contiguous()
             # Flatten the tokens
-            loss = self.loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
+            loss = self.loss_fct(shift_logits.view(-1, shift_logits.size(-1)),
+                                 shift_labels.view(-1))
 
-            return CausalLMOutputWithDomainIDs(
-                loss=loss,
-                logits=lm_logits,
-                past_key_values=None,
-                hidden_states=None,
-                attentions=None,
-                domain_ids=domain_ids)
+            return CausalLMOutputWithDomainIDs(loss=loss,
+                                               logits=lm_logits,
+                                               past_key_values=None,
+                                               hidden_states=None,
+                                               attentions=None,
+                                               domain_ids=domain_ids)
         else:
             return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
-            lm_logits = super().forward({"input_ids": input_ids,"sequence_id": sequence_id}).logits
+            lm_logits = super().forward({
+                "input_ids": input_ids,
+                "sequence_id": sequence_id
+            }).logits
 
             loss = None
             pertoken_loss = None
@@ -227,8 +236,11 @@ class MPTModel(ComposerMPTCausalLM):
                 shift_labels = labels[:, 1:].contiguous()
                 # Flatten the tokens
                 ignore_index = -100
-                pertoken_loss = self.pertoken_loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
-                pertoken_loss = pertoken_loss.view(shift_labels.size(0), shift_labels.size(1))
+                pertoken_loss = self.pertoken_loss_fct(
+                    shift_logits.view(-1, shift_logits.size(-1)),
+                    shift_labels.view(-1))
+                pertoken_loss = pertoken_loss.view(shift_labels.size(0),
+                                                   shift_labels.size(1))
                 token_mask = shift_labels.ne(ignore_index).float()
 
                 loss = pertoken_loss.sum() / token_mask.sum()
@@ -251,11 +263,13 @@ class MPTModel(ComposerMPTCausalLM):
                         domain_ids=domain_ids,
                         return_pertoken_losses=True,
                     )
-                    reference_pertoken_loss = reference_outputs['pertoken_loss']
+                    reference_pertoken_loss = reference_outputs[
+                        'pertoken_loss']
 
             if not return_dict:
-                output = (lm_logits, None, None, None, domain_ids, pertoken_loss, reference_pertoken_loss, token_mask) 
-                return ((loss,) + output) if loss is not None else output
+                output = (lm_logits, None, None, None, domain_ids,
+                          pertoken_loss, reference_pertoken_loss, token_mask)
+                return ((loss, ) + output) if loss is not None else output
 
             return CausalLMOutputWithDomainIDs(
                 loss=loss,
@@ -267,6 +281,7 @@ class MPTModel(ComposerMPTCausalLM):
                 pertoken_loss=pertoken_loss,
                 reference_pertoken_loss=reference_pertoken_loss,
                 token_mask=token_mask)
+
 
 def load_mpt_model(model_name, tokenizer, ckpt_path=None):
     init_context = contextlib.nullcontext()
@@ -295,6 +310,24 @@ def load_mpt_model(model_name, tokenizer, ckpt_path=None):
                 "d_model": 1024,
                 "n_heads": 16,
                 "n_layers": 16,
+                "expansion_ratio": 4,
+                "max_seq_len": 2048,
+                "vocab_size": 50432,
+                "no_bias": True,
+                "loss_fn": "torch_crossentropy",
+                "attn_config": {
+                    "alibi": True,
+                    "attn_impl": "triton",
+                    "clip_qkv": False,
+                    "attn_uses_sequence_id": True
+                }
+            })
+        elif model_name == "mpt-1B":
+            model_cfg = om.create({
+                "name": "mpt_causal_lm",
+                "d_model": 2048,
+                "n_heads": 16,
+                "n_layers": 32,
                 "expansion_ratio": 4,
                 "max_seq_len": 2048,
                 "vocab_size": 50432,
---
+++ scripts/runs/run_pile_mpt_final_default_280M.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+#
+# Sample baseline model run of a 280M model with the same number of non-embedding parameters as the 280M model in the DoReMi paper. Not the same as DoReMi paper since the paper uses 256k vocab size.
+#
+
+
+# load global parameters
+source constants.sh
+pip install -e .
+
+mkdir -p $CACHE
+export HF_HOME=$CACHE
+export TRANSFORMERS_CACHE=$CACHE
+export HF_DATASETS_CACHE=$CACHE
+export HF_DATASETS_IN_MEMORY_MAX_SIZE=0
+export TORCH_EXTENSIONS_DIR=$CACHE
+export TMPDIR=$CACHE
+export WANDB_DIR=${CACHE}/wandb
+export WANDB_PROJECT="doremi-ablate-repro"
+
+PREPROCESSED_DATA=${PREPROCESSED_PILE_DIR}
+PREPROCESSED_CACHE=${CACHE}/preprocessed_cache/perdomain_pile_preprocessed
+
+if [ ! -d "${PREPROCESSED_CACHE}" ]; then
+    mkdir -p ${CACHE}/preprocessed_cache
+    cp -r ${PREPROCESSED_DATA} ${PREPROCESSED_CACHE}
+fi
+
+arg=${1:-""} # set to eval to run eval
+
+if [[ "${arg}" == "eval" ]]; then
+    ADDITIONAL_ARGS="--evaluation_strategy steps --per_device_eval_batch_size 32 --do_train false --remove_unused_columns=False"
+else
+    ADDITIONAL_ARGS=""
+fi
+
+
+NAME=complete_replicate_pile_neox_final_default_280M_50kvocab
+accelerate launch \
+    --config_file accelerate_config.yml \
+    --multi_gpu \
+    --num_machines 4 \
+    --num_processes 32 \
+    --machine_rank $NODE_RANK \
+    --main_process_ip $MASTER_ADDR \
+    --main_process_port $MASTER_PORT \
+    doremi/train.py \
+    --dataset_name pile \
+    --model_name mpt-280M \
+    --tokenizer_name EleutherAI/gpt-neox-20b \
+    --do_train \
+    --cache_dir ${CACHE} \
+    --domain_config_path configs/pile_default_neox_50kvocab.json \
+    --output_dir ${MODEL_OUTPUT_DIR}/${NAME} \
+    --max_token_length 1024 \
+    --per_device_train_batch_size 16 \
+    --gradient_accumulation_steps 1 \
+    --dataloader_num_workers 8 \
+    --max_steps 200000 \
+    --evaluation_strategy no \
+    --save_strategy steps \
+    --save_steps 1000 \
+    --learning_rate 1e-3 \
+    --lr_end 1e-4 \
+    --weight_decay 0.01 \
+    --max_grad_norm 1.0 \
+    --adam_epsilon 1e-8 \
+    --lr_scheduler_name linear_warmup_cosine \
+    --warmup_ratio 0.06 \
+    --run_name ${NAME} \
+    --seed 17 \
+    --logging_strategy steps \
+    --logging_steps 100 \
+    --logging_first_step \
+    --report_to wandb \
+    --optim adafactor \
+    --adam_beta1 0.9 \
+    --adam_beta2 0.99 \
+    --bf16 \
+    ${ADDITIONAL_ARGS}
